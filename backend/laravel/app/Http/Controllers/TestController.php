73dda96a3b6f9cc2927369b87cb5f4e5d0e6a420
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-30 22:31:32 +0300

    Backend <> first api : finish first api

--- backend/laravel/app/Http/Controllers/TestController.php
+++ backend/laravel/app/Http/Controllers/TestController.php
@@ -9,30 +9,45 @@ class TestController extends Controller
     function sortString($string){
         try {  
             $string = str_split($string);
-            $stringSorted=sort($string);
-            $stringSorted=implode($string);
-            $string_numbers="";
-            $string_lower="";
-            $string_upper="";
-            for($i=0;$i<strlen($stringSorted);$i++){
-                if(is_numeric($stringSorted[$i])){
-                    $string_numbers=$string_numbers.$stringSorted[$i];
+            $sorted=sort($string);
+            $sorted=implode($string);
+            $numbers="";
+            $lower="";
+            $upper="";
+            for($i=0;$i<strlen($sorted);$i++){
+                if(is_numeric($sorted[$i])){
+                    $numbers=$numbers.$sorted[$i];
                 }
             }
-            for($i=0;$i<strlen($stringSorted);$i++){
-                if(!ctype_upper($stringSorted[$i]) && !is_numeric($stringSorted[$i])){
-                    $string_lower=$string_lower.$stringSorted[$i];
+            for($i=0;$i<strlen($sorted);$i++){
+                if(!ctype_upper($sorted[$i]) && !is_numeric($sorted[$i])){
+                    $lower=$lower.$sorted[$i];
                 }
             }
-            for($i=0;$i<strlen($stringSorted);$i++){
-                if(ctype_upper($stringSorted[$i]) && !is_numeric($stringSorted[$i])){
-                    $string_upper=$string_upper.$stringSorted[$i];
+            for($i=0;$i<strlen($sorted);$i++){
+                if(ctype_upper($sorted[$i]) && !is_numeric($sorted[$i])){
+                    $upper=$upper.$sorted[$i];
                 }
             }
-            
+            $merge_upper_lower=$lower.$upper;
+            $arrange_string="";
+            for($i=0;$i<strlen($merge_upper_lower);$i++){ 
+                if(ctype_upper($merge_upper_lower[$i])){
+                    break;
+                }
+                $lower_character=$merge_upper_lower[$i];
+
+                for($j=0;$j<strlen($upper);$j++){
+                    if(strcasecmp($lower_character, $upper[$j])==0){
+                        $lower_character=$lower_character.$upper[$j];
+                    }
+                }
+                $arrange_string=$arrange_string.$lower_character;
+            }
+            $arrange_string=$arrange_string.$numbers;
+            return $arrange_string;
         }
           
-          //catch exception
         catch(Exception $e) {
             return "Please try to enter a valid string.";
         }
