2de5754674fc7f5a52cc6fda7cd7a767ed6cf284
Houssein Droubi <housseinalialdroubi@gmail.com>
2022-09-30 21:43:52 +0300

    Backend <> first api: retrieve all upper and lower case from string

--- backend/laravel/app/Http/Controllers/TestController.php
+++ backend/laravel/app/Http/Controllers/TestController.php
@@ -12,12 +12,24 @@ class TestController extends Controller
             $stringSorted=sort($string);
             $stringSorted=implode($string);
             $string_numbers="";
+            $string_lower="";
+            $string_upper="";
             for($i=0;$i<strlen($stringSorted);$i++){
                 if(is_numeric($stringSorted[$i])){
                     $string_numbers=$string_numbers.$stringSorted[$i];
                 }
             }
-            return $string_numbers;
+            for($i=0;$i<strlen($stringSorted);$i++){
+                if(!ctype_upper($stringSorted[$i]) && !is_numeric($stringSorted[$i])){
+                    $string_lower=$string_lower.$stringSorted[$i];
+                }
+            }
+            for($i=0;$i<strlen($stringSorted);$i++){
+                if(ctype_upper($stringSorted[$i]) && !is_numeric($stringSorted[$i])){
+                    $string_upper=$string_upper.$stringSorted[$i];
+                }
+            }
+            
         }
           
           //catch exception
